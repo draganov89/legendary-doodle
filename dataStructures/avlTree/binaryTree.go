8ac7ea0c15ffbbecde967d29c5aea8a04bca0e5d
draganov <draganov89@mail.bg>
2020-02-05 00:54:44 +0200

    Remove method not entirely finished

--- dataStructures/avlTree/binaryTree.go
+++ dataStructures/avlTree/binaryTree.go
@@ -6,7 +6,7 @@ type AVLTree struct {
 	root *treeNode
 	compare func(a, b interface{}) int
 	count int
-	//todo: test Count() !
+	//todo: test Count()o !
 }
 
 func ConstructAVLTree(comparator func(a, b interface{}) int) *AVLTree {
@@ -88,7 +88,7 @@ func (t *AVLTree) add(current *treeNode, newNode *treeNode) *treeNode, bool {
 	return current
 }
 
-func (t *AVLTree) remove(val interface{}) (*treeNode, bool) {
+func (t *AVLTree) Remove(val interface{}) (*treeNode, bool) {
 	var parent *treeNode
 	current := t.root
 
@@ -105,23 +105,52 @@ func (t *AVLTree) remove(val interface{}) (*treeNode, bool) {
 			parent = current
 			current = current.Left
 		} else {
-			// here it is !     val == current.Value
 			break
 		}
+	}
 
-		// parent is parent of Current
-		// remove Current
+    if current.Left == nil {
+    	if current.Right == nil {
+    		replaceChild(parent, current, nil)
+    	}
+    	replaceChild(parent, current, current.Right)
+	} else if current.Right == nil { // only Left child    
+		replaceChild(parent, current, current.Left)
+	} else {
+		// case 3 - current has 2 children 
+
+		//find ancestor
+		ancestor := current.Right
+		var previous *treeNode
+		for ancestor.Left != nil {
+			ancestor = ancestor.Left
+		}
+
+
+		//ancestorsRight := ancestor.Right
+
+		// here I have the correct ancestor
+
+	}
 
 		//find successor
 		// 3 cases -> 
-		successor := current
+		//successor := current
 
-		for {
-
-		}
+	
 
 		
+	t.count--;
+}
+
+//can be improved. Try to move the child's 
+//logic inside - if has only right or left ...
+func replaceChild(parent, child, replace *treeNode) {
+	if parent.Left = child {
+		parent.Left = replace
+		return 
 	}
+	parent.Right = replace
 }
 
 func setHeight(node *treeNode) int {
